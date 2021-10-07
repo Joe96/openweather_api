a0f98ebf7928b6dd7cc51c95ce2e207856c83581
Joel Afamefune <jafamefune@gmail.com>
2021-10-06 23:41:58 -0500

    Populate page with api data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 var userInputValue = $('#userInput').attr('value');
-
 var {lat} = location;
 var {lon} = location;
+var uvIndex = $("#uvIndex");
 var apiBaseUrl = 'https://api.openweathermap.org/';
 
 function getCoords(cityName){
     console.log(cityName);
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ef394b3dac59c5d80dbb39281ef08319`;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=ef394b3dac59c5d80dbb39281ef08319`;
     fetch(requestUrl)
     .then(function(response){
         return response.json();
@@ -15,8 +15,12 @@ function getCoords(cityName){
         console.log(data)
         lat = data.coord.lat;
         lon = data.coord.lon;
-        console.log(lon) 
-        console.log(lat)
+
+        $("#cityname").html(data["name"]+moment(data.dt,"X").format("( MM/DD/YYYY )") +"<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'/>");
+        $("#temp").text("Temp: " + data["main"].temp + " F");
+        $("#wind").text("Wind: " + data["wind"].speed + " mph");
+        $("#humidity").text("humidity: " + data["main"].humidity+ " %");
+
         getWeather();
     });
 }
@@ -26,16 +30,23 @@ function getWeather(){
     fetch(requestUrl)
     .then(function(response){
         return response.json();
+        
     })
-    .then(function(data){
-        console.log(data);
-    });
+    .then(function(data) {
+        for(var i=0; i<5; i++){
+            $("#date"+i).html(moment(data.daily[i].dt,"X").format("( MM/DD/YYYY )") +"<img src='http://openweathermap.org/img/w/"+data.daily[i].weather[0].icon+".png'/>");
+            $("#temp"+i).text("Temp: " + data.daily[i].temp.day + " F");
+            $("#wind"+i).text("Wind: " + data.daily[i].wind_speed + " mph");
+            $("#humidity"+i).text("humidity: " + data.daily[i].humidity+ " %");
+        }
+    })
+    updateStorage(data["name"]);
+    futureForecast(data["coord"].lat, data["coord"].lon);
 }
 
 $(document).ready(function() {
     $("#userInput").on('keyup', function (event) {
         if (event.keyCode === 13) {
-            console.log("Enter key pressed!!!!!");
             var cityName = $("#userInput").val();
             getCoords(cityName)
         }
